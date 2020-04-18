fb37597af15aca28204741c9f6b86c56945898eb
Lavanya2989 <lapsna@gmail.com>
2020-04-18 16:25:06 +1200

    API feature file and coding

--- ConsoleApp4/APITestsSteps.cs
+++ ConsoleApp4/APITestsSteps.cs
@@ -5,32 +5,30 @@ using TechTalk.SpecFlow;
 
 namespace ConsoleApp4
 {
-    [Binding]
-       public class APITestsSteps
-    {
-        RestClient restclient;
-        RestRequest req;
-        IRestResponse response;
-        string JsonRequest = @"{ ""addressLine1"": ""14"",""addressLine2"": ""street"",""city"": ""wlg"",""comment"": ""sdh"",""email"": ""shar@gmail.com"",""firstName"": ""raj"",""id"": 0,""itemNumber"": 0,""lastName"": ""shar"",""phone"": ""6789012345"", ""state"": ""AL"",""zipCode"": ""456789""}";
-
-        [Given(@"I have correct billing order")]
-        public void GivenIHaveCorrectBillingOrder()
+       [Binding]
+        public class APITestsSteps
         {
-            RestClient client = new RestClient("http://localhost:8080");
-        }
+            RestClient restclient;
+            RestRequest req;
+            IRestResponse response;
+            string JsonRequest = @"{ ""addressLine1"": ""14"",""addressLine2"": ""street"",""city"": ""wlg"",""comment"": ""sdh"",""email"": ""shar@gmail.com"",""firstName"": ""raj"",""id"": 0,""itemNumber"": 0,""lastName"": ""shar"",""phone"": ""6789012345"", ""state"": ""AL"",""zipCode"": ""456789""}";
+            [Given(@"I have correct billing order")]
+            public void GivenIHaveCorrectBillingOrder()
+            {
+                restclient = new RestClient("http://localhost:8080");
+            }
+            [When(@"I send this order to API via Post request")]
+            public void WhenISendThisOrderToAPIViaPostRequest()
+            {
+                req = new RestRequest("/BillingOrder", Method.POST);
+                req.AddJsonBody(JsonRequest);
+                response = restclient.Execute(req);
+            }
 
-        [When(@"I send this order to API via Post request")]
-        public void WhenISendThisOrderToAPIViaPostRequest()
-        {
-            req = new RestRequest("/BillingOrder", Method.POST);
-            req.AddJsonBody(JsonRequest);
-            response = restclient.Execute(req);
-        }
-
-        [Then(@"Receive response with correct http address")]
-        public void ThenReceiveResponseWithCorrectHttpAddress()
-        {
-            TestContext.WriteLine("ResponseCode :" + response.Content);
+            [Then(@"Receive response with correct http address")]
+            public void ThenReceiveResponseWithCorrectHttpAddress()
+            {
+                TestContext.WriteLine("ResponseCode :" + response.Content);
+            }
         }
     }
-}
